ebc703e6822e7212f17aaf2c68e93909ea47ea14
cristobal <c.ramos07@ufromail.cl>
2025-03-27 20:23:29 -0300

    como

--- src/main.java
+++ src/main.java
@@ -1,7 +1,7 @@
 public class main {
     public static void main(String[] args){
         System.out.println("COMO SE USA GITHUB");
-        System.out.println("ayuda");
+        System.out.println("ayudaa");
 
     }
 }
